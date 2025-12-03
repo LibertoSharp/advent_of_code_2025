7071abff9bfc0bfa1679e0ee288195618396eadc
Liberto <liberto.sharp@gmail.com>
2025-12-03 23:51:21 +0100

    day 03

---
+++ day03/part1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char * get_max(char *s, int n)
+{
+    char *max = s;
+    int i = 0;
+    while (s[i] && i < n)
+    {
+        if (s[i] > *max) max = s+i;
+        i++;
+    }
+    return max;
+}
+
+int main(void)
+{
+    FILE *fptr = fopen("input.txt", "r");
+    char buff[100];
+    int result = 0; 
+    while (fgets(buff, 102, fptr))
+    {
+        char *max_chr = get_max(buff, 99);
+        int first_digit = *max_chr - 48;
+        *max_chr = 48;
+        max_chr = get_max(max_chr, 100);
+        int second_digit = *max_chr - 48;
+        result += first_digit * 10 + second_digit;
+    }
+    printf("result: %d\n", result);
+}
---
+++ day03/part2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char * get_max(char *s)
+{
+    char *max = s;
+    int i = 0;
+    while (s[i])
+    {
+        if (s[i] > *max) max = s+i;
+        i++;
+    }
+    return max;
+}
+
+int main(void)
+{
+    FILE *fptr = fopen("input.txt", "r");
+    char buff[100];
+    unsigned long long result = 0; 
+    while (fgets(buff, 102, fptr))
+    {
+        unsigned long long battery = 0;
+        char *max_chr = buff;
+        for (int i = 0; i < 12; i++)
+        {
+            char saved = buff[89+i];
+            buff[89+i] = 0;
+            max_chr = get_max(max_chr);
+            buff[89+i] = saved;
+            battery = battery * 10 + (*max_chr - 48);
+            *max_chr = 48;
+        }
+        result += battery;
+    }
+    printf("result: %lld\n", result);
+}
